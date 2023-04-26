61e9fb6823c0f28316cc971f27a8a070b80fd7f0
dimi133 <124206879+dimi133@users.noreply.github.com>
2023-04-26 22:19:13 +0200

    Add files via upload

---
+++ Level 1/script2(3).js
@@ -0,0 +1,14 @@
+let newP = document.querySelectorAll('p');
+for(let i=0; i<newP.length; i++){
+    newP[i].addEventListener('click', changeColor);
+
+
+function changeColor(){
+    newP[i].classList.toggle('active');
+}
+// let active1 = document.querySelectorAll('.active');
+//     active1.style.width = "50px";
+//     active1.style.height = '50px';
+//     active1.style.backgroundColor = 'black';
+//     active1.style.color = 'brown';
+ }
